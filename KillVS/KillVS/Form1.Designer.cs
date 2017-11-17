f160d5573afbe78755f531e5d5e04b6fb12e0b18
Samiris <samiris.alvaro@gmail.com>
2017-11-17 13:24:54 -0200

    Add files via upload

---
+++ KillVS/KillVS/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace KillVS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbxItens = new System.Windows.Forms.ListBox();
+            this.btnTodos = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbxItens
+            // 
+            this.lbxItens.FormattingEnabled = true;
+            this.lbxItens.ItemHeight = 16;
+            this.lbxItens.Location = new System.Drawing.Point(13, 13);
+            this.lbxItens.Name = "lbxItens";
+            this.lbxItens.Size = new System.Drawing.Size(330, 196);
+            this.lbxItens.TabIndex = 0;
+            // 
+            // btnTodos
+            // 
+            this.btnTodos.Location = new System.Drawing.Point(172, 218);
+            this.btnTodos.Name = "btnTodos";
+            this.btnTodos.Size = new System.Drawing.Size(75, 23);
+            this.btnTodos.TabIndex = 1;
+            this.btnTodos.Text = "Todos";
+            this.btnTodos.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnTodos.UseVisualStyleBackColor = true;
+            this.btnTodos.Click += new System.EventHandler(this.btnTodos_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(253, 218);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(90, 23);
+            this.btnCancelar.TabIndex = 2;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 253);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnTodos);
+            this.Controls.Add(this.lbxItens);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "KillVS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbxItens;
+        private System.Windows.Forms.Button btnTodos;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
+
---
+++ KillVS/KillVS/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace KillVS
+{
+    public partial class Form1 : Form
+    {
+        private List<Process> processos = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            lbxItens.DoubleClick += new EventHandler(lbxItens_DoubleClick);
+        }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool IsIconic(IntPtr hWnd);
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                processos = Process.GetProcesses().Where(p => p.MainWindowTitle.Contains("Microsoft Visual Studio")).ToList();
+
+                if (processos != null)
+                {
+                    if (processos.Count > 1)
+                    {
+                        foreach (var processo in processos)
+                        {
+                            //IntPtr wHnd = processo.MainWindowHandle;
+                            //IsIconic(wHnd);
+                            lbxItens.Items.Add(processo.Id + "_" + processo.MainWindowTitle);
+                        }
+                    }
+                    else
+                    {
+                        DialogResult result = MessageBox.Show("Certeza?", "Confirmar!", MessageBoxButtons.YesNo);
+
+                        if (result == DialogResult.Yes)
+                        {
+                            processos[0].Kill();
+                        }
+
+                        this.Close();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                this.Close();
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnTodos_Click(object sender, EventArgs e)
+        {
+            foreach (var processo in processos)
+            {
+                processo.Kill();
+            }
+
+            this.Close();
+        }
+
+        private void lbxItens_DoubleClick(object sender, EventArgs e)
+        {
+            if (lbxItens.SelectedItem != null)
+            {
+                int idProcesso = Convert.ToInt32(lbxItens.SelectedItem.ToString().Split('_')[0]);
+                var processo = processos.Where(x => x.Id == idProcesso).SingleOrDefault();
+
+                if (processo != null)
+                {
+                    processo.Kill();
+                    lbxItens.Items.Remove(lbxItens.SelectedItem);
+                }
+            }
+        }
+    }
+}
